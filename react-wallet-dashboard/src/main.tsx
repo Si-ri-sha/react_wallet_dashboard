b5831713e5d2f47773088c0e3375ec855918ab86
Si-ri-sha <sirishasirisha806@gmail.com>
2025-05-22 18:55:41 +0530

    feat: Integrating Tailwind and PostCSS pipeline

--- react-wallet-dashboard/src/main.tsx
+++ react-wallet-dashboard/src/main.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
+
 import { WalletProvider } from "./context/Walletprovider.tsx";
 import { ThemeProvider } from "./context/ThemeContext";
 
